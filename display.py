ee6bde10448a2c91339b91b56fc41de37a298526
Mat Moore <matmoore@users.noreply.github.com>
2010-08-15 14:10:37 +0100

    Removed pygame stuff

--- display.py
+++ display.py
@@ -1,176 +1,11 @@
 import game
 import board
-import pygame
-from pygame.locals import *
 import geometry
 import sys
 import observer
 import multilogger
 import rules
 import wx
-
-class GridViewPygame(observer.Observable):
-	'''Draw grids using pygame'''
-
-	def __init__(self, grid, surface, scale=None, rotation=0, zoom_to_fit=False, join_connected=False, highlight_connected=True, colors = {}, mainScreen=False, offset=(20,20)):
-		observer.Observable.__init__(self)
-		grid_width = max([x for x,y in grid.points])
-		grid_height = max([y for x,y in grid.points])
-		width = surface.get_width()-2*offset[0]
-		height = surface.get_height()-2*offset[1]
-
-		if zoom_to_fit:
-			scale = min(width/float(grid_width), height/float(grid_height))
-		elif scale is None:
-			scale = 1
-
-		self.scale = scale
-		self.offset = offset
-		self.grid = grid
-		self.surface = surface
-		self.grid.register_listener(self.draw)
-		self.highlighted = None
-
-		self.highlight_connected = highlight_connected
-		self.join_connected = join_connected
-
-		if colors:
-			self.colors = colors
-		else:
-			self.colors = {
-					'black':(0,0,0),
-					'white':(255,255,255)
-			}
-
-		# Flag to specify if we are drawing to the screen surface
-		self.mainScreen = mainScreen
-		self.draw()
-
-	def draw(self, *args):
-		'''Draw the grid'''
-		debug('Redrawing grid')
-		scale = self.scale
-		self.surface.fill((255,255,255))
-
-		# This actually draws the line for each connection twice but never mind.
-		# It also doesn't care about whether edge connections should be visible.
-		if self.join_connected:
-			for a,bs in self.grid.connections.items():
-				for b in bs:
-					ax,ay = self.grid_to_view(a)
-					bx,by = self.grid_to_view(b)
-					pygame.draw.line(self.surface, (0,0,0), (ax,ay), (bx,by),4)
-
-		for p in self.grid.points:
-			x,y = p
-			value = self.grid.get_point(x,y)
-			x,y = self.grid_to_view(p)
-
-			if value is not None and value[0] in self.colors:
-				pygame.draw.circle(self.surface, self.colors[value[0]], (int(x),int(y)), 15)
-			else:
-				pygame.draw.circle(self.surface, (0,0,0), (int(x),int(y)), 5)
-
-		if self.highlight_connected and self.highlighted:
-			debug('Highlighting connections for %s' % str(self.highlighted))
-			for c in self.grid.connections[self.highlighted]:
-				x,y = self.grid_to_view(c)
-				pygame.draw.circle(self.surface, (0,255,0), (int(x),int(y)), 2)
-
-		if self.mainScreen:
-			pygame.display.flip()
-
-		self.notify()
-
-	def grid_to_view(self,pos):
-		'''Convert grid coordinated to view ones'''
-		x,y = pos
-		ox,oy = self.offset
-		return(x*self.scale+ox, y*self.scale+oy)
-
-	def view_to_grid(self, pos):
-		'''Convert view coordinates to grid ones'''
-		x,y = pos
-		ox,oy = self.offset
-		x = int(round((x-ox)/self.scale))
-		y = int(round((y-oy)/self.scale))
-		return (x,y)
-
-	def on_click(self, pos):
-		pass
-
-class GameViewPygame(GridViewPygame):
-	'''An interactive go board'''
-	def __init__(self, game_controller, surface, scale=None, rotation=0, zoom_to_fit=False, join_connected=False, highlight_connected=True, colors = {}):
-		GridViewPygame.__init__(self, game_controller.game.board.grid, surface, scale, rotation, zoom_to_fit, join_connected, highlight_connected, colors)
-		self.controller = game_controller
-		self.accept_moves = game_controller.accept_local_moves
-
-	def on_click(self, pos):
-		'''Callback called when the user clicks in the window'''
-		# Ignore clicks outside the board
-		# TODO: fix board to include all the top/left of stones properly
-		x,y = pos
-
-		if x < 0 \
-		or y < 0 \
-		or x > self.surface.get_width() \
-		or y > self.surface.get_height():
-			return
-
-		pos = self.view_to_grid(pos)
-
-		if self.controller.accept_local_moves:
-			try:
-				debug('Playing %s,%s...' % pos)
-				self.controller.play_move(pos)
-			except game.NotYourTurnError:
-				debug('Not your turn')
-			except board.BoardError:
-				debug('Cannot place a stone there')
-			except rules.KoError:
-				debug('Invalid move (ko)')
-			except rules.SuicideError:
-				debug('Invalid move (suicide)')
-			except rules.InvalidMove:
-				debug('Invalid move')
-
-class GameGUIPygame():
-	'''GUI for playing a game of go'''
-	def __init__(self,game_controller,screen,board_size,offset,**kwargs):
-		self.screen = screen
-		board_surface = pygame.Surface(board_size)
-		self.game_view = GameViewPygame(game_controller,board_surface,**kwargs)
-		self.game_view.register_listener(self.draw)
-
-		b_left,b_top = offset
-		b_width,b_height = board_size
-		self.layout = {
-				'board': (b_left,b_top,b_left+b_width,b_top+b_height)
-		}
-
-		self.draw()
-
-	def screen_to_view(self,pos,view='board'):
-		'''Convert screen coordinates into coordinates for a view'''
-		x,y = pos
-		left = self.layout[view][1]
-		top = self.layout[view][0]
-		return (x-left, y-top)
-
-	def draw(self, *args):
-		'''Draw the gui'''
-		self.screen.fill((255,255,255))
-
-		# Draw the board in the middle
-		board_pos = self.layout['board'][:2]
-		self.screen.blit(self.game_view.surface, board_pos)
-		pygame.display.flip()
-
-	def on_click(self,pos):
-		'''Forward on_click event down to child views'''
-		x,y = self.screen_to_view(pos)
-		self.game_view.on_click(pos)
 
 class PlayerPanel(wx.Panel):
 	'''Show details about the players'''
@@ -312,12 +147,6 @@ class BoardViewWx(wx.Panel):
 				gc.SetBrush(wx.Brush("black"))
 				gc.DrawEllipse(x-pointSize/2, y-pointSize/2, pointSize, pointSize)
 
-#		if self.highlight_connected and self.highlighted:
-#			debug('Highlighting connections for %s' % str(self.highlighted))
-#			for c in self.grid.connections[self.highlighted]:
-#				x,y = self.grid_to_view(c)
-#				pygame.draw.circle(self.surface, (0,255,0), (int(x),int(y)), 2)
-
 	def GridToView(self,pos):
 		'''Convert grid coordinated to view ones'''
 		x,y = pos
@@ -402,7 +231,7 @@ class PlayableBoardWx(BoardViewWx):
 
 		pos = event.GetPositionTuple()
 		pos = self.ViewToGrid(pos)
-	
+
 		# Ignore clicks in the border area
 		if pos:
 			x,y = pos
@@ -564,33 +393,3 @@ class GoApp(wx.App):
 
 # Get a logger for this module
 debug,info,warning,error = multilogger.logFunctions(__name__)
-
-#Test the display stuff only
-if __name__ == '__main__':
-
-	# Change on click behaviour
-	def on_click(self,pos):
-		'''Highlight the nearest point'''
-		pos = self.view_to_grid(pos)
-		self.highlighted = pos
-		self.draw(False,True)
-	GridViewPygame.on_click = on_click
-
-	#Initialize pygame
-	pygame.init()
-	screen = pygame.display.set_mode((600, 300))
-
-	#Create grid
-	grid = geometry.FoldedGrid(5,5,1,[('N','S'),('E','W')])
-	thegame = game.TwoPlayerGame(board.Board(grid))
-	colors = {thegame.black:(0,0,0),thegame.white:(100,100,100)}
-	view = GridViewPygame(thegame.board.grid, screen, zoom_to_fit=True, join_connected=True, highlight_connected=True, colors=colors, mainScreen=True)
-
-	#Handle events
-	while True:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-			elif event.type == MOUSEBUTTONDOWN:
-				view.on_click(pygame.mouse.get_pos())
--- run_gui.py
+++ run_gui.py
@@ -5,10 +5,8 @@ import display
 import game
 import geometry
 import board
-import pygame
 import sys
 import multilogger
-from pygame.locals import *
 
 # Flesh this out when the ability to configure the game is implemented properly. Later, this function or something similar will be called from within the GUI when the user chooses to begin a game.
 def start_game_simple():
@@ -25,25 +23,6 @@ def start_game_simple():
 
 	return game_controller,black,white
 
-def start_game_pygame():
-	'''Use pygame to display a game of Go'''
-	pygame.init()
-	screen = pygame.display.set_mode((620, 320))
-
-	game_controller,black,white = start_game_simple()
-
-	colors = {black:(0,0,0), white:(200,200,200)}
-	view = display.GameGUIPygame(game_controller, screen, (600,300), (10,10), zoom_to_fit=True, join_connected=True, highlight_connected=True,colors=colors)
-
-	#Handle events
-	while True:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-			elif event.type == MOUSEBUTTONDOWN:
-				view.on_click(pygame.mouse.get_pos())
-
 def start_game_wx():
 	'''Use wxPython to display a game of go'''
 	game_controller,black,white = start_game_simple()
@@ -57,8 +36,4 @@ debug,info,warning,error = multilogger.logFunctions(__name__)
 if __name__ == '__main__':
 
 	info('Starting GUI')
-
-	if len(sys.argv) > 1 and sys.argv[1].lower() == 'pygame':
-		start_game_pygame()
-	else:
-		start_game_wx()
+	start_game_wx()
